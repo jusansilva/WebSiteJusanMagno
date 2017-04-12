ceed3d7c8c60ca9066cc1d00a9243711699ed429
jusansilva <jusanmagno@gmail.com>
2017-04-11 21:26:17 -0300

    Add project files.

---
+++ Jusan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jusan.Startup))]
+namespace Jusan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
